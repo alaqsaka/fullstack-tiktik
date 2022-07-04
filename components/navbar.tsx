bea39d44d6a6b568e34039a5b292438c93bfdfc0
alaqsaka <aqsaabidin15@gmail.com>
2022-07-04 14:58:26 +0700

    initialize navbar, sidebar component, installing tailwind

---
+++ components/navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { GoogleLogout, GoogleLogin } from 'react-google-login'
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
