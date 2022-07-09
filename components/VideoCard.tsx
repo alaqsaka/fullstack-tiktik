faba57a1cb16122bdc2f38385fc67fab7d2433c0
alaqsaka <aqsaabidin15@gmail.com>
2022-07-09 13:24:48 +0700

    video card done

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -75,7 +75,8 @@ const VideoCard: NextPage<IProps> = ({post}) => {
 
           {/* If mouse hover on a video show buttons */}
           {isHover && (
-            <div>
+            <div className='absolute bottom-6 cursor-pointer left-8 md:left-14 
+            lg:left-0 flex gap-10 lg:justify-between w-[100px] md:w-[50px] p-3'>
               {playing ? (
                 <button onClick={onVideoPress}>
                   <BsFillPauseFill className='text-black text-2xl lg:text-4xl'/>
@@ -87,11 +88,11 @@ const VideoCard: NextPage<IProps> = ({post}) => {
               )}
 
               {isVideoMuted ? (
-                <button>
+                <button onClick={() => setIsVideoMuted(false)}>
                   <HiVolumeOff className='text-black text-2xl lg:text-4xl'/>
                 </button>
               ) : (
-                <button>
+                <button onClick={() => setIsVideoMuted(true)}>
                   <HiVolumeUp className='text-black text-2xl lg:text-4xl'/>
                 </button>
               )}
