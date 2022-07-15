6d55c479553c3f45631a7a466147a329cf88fbc5
alaqsaka <aqsaabidin15@gmail.com>
2022-07-15 19:57:58 +0700

    feat: adding style to upload page

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -6,6 +6,7 @@ import { SanityAssetDocument } from '@sanity/client'
 import axios from 'axios'
 import useAuthStore from '../store/authStore'
 import {client} from '../utils/client'
+import { topics } from '../utils/constants'
 
 const Upload = () => {
     const [isLoading, setisLoading] = useState(false)
@@ -34,8 +35,10 @@ const Upload = () => {
     }
 
   return (
-    <div className='flex w-full h-full'>
-        <div className='bg-white rounded-lg'>
+    <div className='flex w-full h-full absolute left-0 top-[60px] mb-10
+    pt-10 lg:pt-20 bg-[#F8F8F8] justify-center'>
+        <div className='bg-white rounded-lg xl:h-[80vh] flex gap-6 flex-wrap justify-between items-center
+        p-14 pt-6 w-[60%]'>
             <div>
                 <div>
                     <p className='text-2xl font-bold'>Upload Video</p>
@@ -91,6 +94,43 @@ const Upload = () => {
                             }
                         </div>
                     )}
+                    {wrongFileType && (
+                        <p className='text-center text-xl text-red-400 font-semibold mt-4 w-[250px]'>
+                            Please select a video file
+                        </p>
+                    )}
+                </div>
+            </div>
+            <div className='flex flex-col gap-3 pb-10'>
+                <label htmlFor="caption" className='text-md font-medium'>Caption</label>
+                <input type="text" value="" onChange={() => {}}  
+                className="rounded outline-none text-md border-2 border-gray-200 p-2"/>
+                <label htmlFor="category" className='text-md font-medium'>Choose a Category</label>
+                <select name="" id="" onChange={() => {}}
+                        className="outline-none border-2 border-gray-200 text-md
+                        capitalize lg:p-4 p-2 rounded cursor-pointer"
+                >
+                    {topics.map((topic) => (
+                        <option key={topic.name} 
+                        className="outline-none bg-white text-gray-700 
+                        capitalize text-md p-2 
+                        hover:bg-slate-300"
+                        value={topic.name}>{topic.name}</option>
+                    ))}
+                </select>
+                <div className='flex gap-6 mt-10'>
+                    <button onClick={() =>{}} type="button"
+                        className='border-gray-300 border-2 text-md font-medium p-2 rounded w-28
+                        lg:w-44 outline-none' 
+                        >
+                    Discard
+                    </button>
+                    <button onClick={() =>{}} type="button"
+                        className='bg-[#F51997] text-white text-md font-medium p-2 rounded w-28
+                        lg:w-44 outline-none' 
+                        >
+                    Post
+                    </button>
                 </div>
             </div>
         </div>
