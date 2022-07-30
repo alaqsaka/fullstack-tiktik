3d079afde2640a64f872b7458849b381281e63be
alaqsaka <aqsaabidin15@gmail.com>
2022-07-30 17:52:31 +0700

    slicing search button

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
@@ -13,6 +13,16 @@ import { GoogleLogout } from 'react-google-login'
 
 const Navbar = () => {
   const { userProfile, addUser, removeUser } = useAuthStore();
+  const [searchValue, setSearchValue] = useState('')
+  const router = useRouter()
+
+  const handleSearch = (e: {preventDefault: () => void}) => {
+    e.preventDefault()
+
+    if(searchValue) {
+      router.push(`/search/${searchValue}`)
+    }
+  }
 
   return (
     <div className='w-full flex justify-between items-center border-b-2 border-gray-200 px-4 py-2'>
@@ -21,7 +31,30 @@ const Navbar = () => {
           <Image className='cursor-pointer' src={Logo} alt="TikTik Logo" layout='responsive'></Image>
         </div>
       </Link>
-      <div>Search</div>
+
+      <div className='relative hidden md:block'>
+        <form action="" onSubmit={handleSearch} className="absolute md:static top-10 left-20 
+        bg-white">
+          <input 
+            type="text"
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+            placeholder="Search accounts and videos"
+            className='bg-primary p-3 md:text-md 
+            font-medium border-2 
+            border-gray-100 focus:outline-none 
+            focus:border-2 focus:border-gray-300
+            w-[300px] md:w-[350px] rounded-full md:top-0'
+          />
+          <button 
+            onClick={() => {}}
+            className='absolute md-right-5 right-6 top-4 
+            border-l-2 border-gray-300 pl-4 text-2xl text-gray-400'>
+            <BiSearch />
+          </button>
+        </form>
+      </div>
+
       <div>
         {userProfile ? (
           <div className='flex gap-5 md:gap-10'>
