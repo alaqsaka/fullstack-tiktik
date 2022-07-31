c5fc101264cffa3186feb286b3dbcee2a1379fcd
alaqsaka <aqsaabidin15@gmail.com>
2022-07-31 14:51:40 +0700

    adding page title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import { useState, useEffect } from "react";
 import Sidebar from '../components/Sidebar';
 import Navbar from '../components/Navbar';
 import { GoogleOAuthProvider } from '@react-oauth/google'
+import Head from 'next/head';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   const [isSSR, setIsSSR] = useState(true);
@@ -17,6 +18,10 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
 
   return (
     <GoogleOAuthProvider clientId={`${process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN}`}>
+      <Head>
+        <title>TikTik - Enjoy Your Day</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <div className='xl:w-[1200px] m-auto overflow-hidden h-[100vh]'>
         <Navbar />
         <div className="flex gap-6 md:gap-20">
